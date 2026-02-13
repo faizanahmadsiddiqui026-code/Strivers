bb6a2ce188e8872afb6daa074367506e6ee0de38
Faizan_05 <faizanahmadsiddiqui026@gmail.com>
2026-02-13 17:19:40 +0530

    Update server.js
    Update Origin

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(cors({
-  origin: 'https://strivers-three.vercel.app/',
+  origin: 'https://strivers-pmvi.vercel.app//',
   credentials: true
 }));
 
