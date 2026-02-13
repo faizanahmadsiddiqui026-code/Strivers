a3edf383ee46bfef114d5c3c263f7744fcb46a31
Faizan_05 <faizanahmadsiddiqui026@gmail.com>
2026-02-13 17:10:44 +0530

    Update CustSignup.jsx
    Changed Origin

--- src/pages/customer/CustSignup.jsx
+++ src/pages/customer/CustSignup.jsx
@@ -34,7 +34,7 @@ export default function CustomerSignup() {
     try {
       setLoading(true);
 
-      const response = await fetch('http://localhost:4000/user/signup', {
+      const response = await fetch('https://strivers-three.vercel.app/user/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
